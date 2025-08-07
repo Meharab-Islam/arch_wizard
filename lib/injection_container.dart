420c59c33767f06e6c370d595367accd22616593
Meharab-Islam <mdnibir178@gmail.com>
2025-08-07 16:53:56 +0600

    Initial commit of Arch Wizard CLI tool

---
+++ lib/injection_container.dart
@@ -0,0 +1,12 @@
+import 'package:get_it/get_it.dart';
+
+// Create a global instance of GetIt for service location
+final sl = GetIt.instance;
+
+Future<void> init() async {
+  // TODO: Register any other app-wide dependencies here (e.g., http.Client)
+
+
+  // The Arch Wizard will add new feature dependencies right below this line.
+  // [DI_GENERATE_HERE]
+}
